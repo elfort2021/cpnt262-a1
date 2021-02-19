ff4294c8c30ee956785362f59f3eb82b1201a3ac
elfort2021 <ellie.forte9@gmail.com>
2021-02-19 12:53:16 -0700

    total revamp haw

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # cpnt262-a1
 
 
-hamburger icon 
-https://icon-icons.com/icon/hamburger-menu/144302#32
+
+Originally started with Hamburger menu that toggles between open and closed. 
+Realized I had only used query selectors and none of the document events or the required input for the assignment . 
+always a classic move. 
+starting over Friday 11:16 am! Here we go. all the code from the hb menu is in my github though- i'll go back and look at it to try and figure out why I went so wrong. Also my foreach function presented two different loop options and I couldnt understand the first one, more hw 
+
+
+
+Switcing to Dark-lightmode toggle.
+
+
+Scared - Color Change
+
+https://fontawesome.com/icons/flushed?style=solid
+
+Smile Icon- Color Change 
+https://fontawesome.com/icons/smile-beam?style=solid
--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-// check ths strict thing in this class // 
-
-// attempting to make a content drawer open, here we go 
-
-let toggleNavStatus = false;
-
-let toggleNav =  function() {
-  let grabSideNav = document.querySelector(".nav-sidebar");
-  let grabSideNavUl = document.querySelector(".nav-sidebar ul");
-  let grabSideNavSpan = document.querySelector(".nav-sidebar span");
-  let grabSideNavLinks = document.querySelectorAll(".nav-sidebar a");
 
 
-  if (toggleNavStatus === false) { 
-    grabSideNavUl.style.visibility = "visible"; 
-    grabSideNav.style.width = "275px"; 
-    grabSideNavSpan.style.opacity = "0.5"; 
+const checkbox = document.getElementById('checkbox');
 
-    // let arrayLength = grabSideNavLinks.length; 
-    // for (let i = 0; i < arrayLength; i++) {
-    //   grabSideNavLinks[i].style.opacity = "1";
-    // }
-
-    // grabSideNavLinks.forEach(element => {
-    //   grabSideNavLinks[1].style.opacity = "1";
-    // });
-    
-    toggleNavStatus = true; 
-  }
-
-
-else if (toggleNavStatus === true) { 
+checkbox.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
   
-  grabSideNav.style.width = "40px"; 
-  grabSideNavSpan.style.opacity = "0"; 
+});
 
-  let arrayLength = grabSideNavLinks.length; 
-  for (let i = 0; i < arrayLength; i++) {
-    grabSideNavLinks[i].style.opacity = "0";
-  }
-
-  // grabSideNavLinks.forEach(element => {
-  //   grabSideNavLinks[1].style.opacity = "1";
-  // });
-  
-  grabSideNavUl.style.visibility = "hidden"; 
-
-  toggleNavStatus = false; 
-}
-
-}
+            
